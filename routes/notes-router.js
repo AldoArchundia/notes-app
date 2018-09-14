c010e8429141b65f30a5bb87a3d23c4fde1729d8
Cald <aldo.archundia@ksquareinc.com>
2018-09-14 11:28:35 -0500

    refactor: Promises -> async , await

--- routes/notes-router.js
+++ routes/notes-router.js
@@ -3,59 +3,53 @@ const express = require("express");
 const NotesRouterFactory = notesService => {
   const notesRouter = express.Router();
 
-  notesRouter.get("/", (req, res) => {
-    notesService
-      .getAll()
-      .then(notes => {
-        res.json({ notes });
-      })
-      .catch(err => {
-        res.status(500).json({ error: "Internal server error" });
-      });
+  notesRouter.get("/", async (req, res) => {
+    try {
+      const notes = await notesService.getAll();
+      res.json({ notes });
+    } catch (err) {
+      res.status(500).json({ error: "Internal server error" });
+    }
   });
 
-  notesRouter.get("/:id", (req, res) => {
-    notesService
-      .getOneById(req.params.id)
-      .then(note => {
-        if (note === null) {
-          res.status(404).json({ message: "Note not found" });
-        } else {
-          res.json({ note });
-        }
-      })
-      .catch(err => {
-        res.status(500).json({ error: "Internal server error" });
-      });
+  notesRouter.get("/:id", async (req, res) => {
+    try {
+      const note = await notesService.getOneById(req.params.id);
+      if (note === null) {
+        res.status(404).json({ message: "Note not found" });
+      } else {
+        res.json({ note });
+      }
+    } catch (err) {
+      res.status(500).json({ error: "Internal server error" });
+    }
   });
 
-  notesRouter.post("/", (req, res) => {
-    notesService
-      .createOne(req.body)
-      .then(note => {
-        res.status(201).json({ note });
-      })
-      .catch(err => {
-        res.status(500).json({ error: "Internal server error" });
-      });
+  notesRouter.post("/", async (req, res) => {
+    try {
+      const note = await notesService.createOne(req.body);
+      res.status(201).json({ note });
+    } catch (err) {
+      res.status(500).json({ error: "Internal server error" });
+    }
   });
 
-  notesRouter.delete("/:id", (req, res) => {
-    notesService
-      .deleteOneById(req.params.id)
-      .then(() => res.status(200).json({}))
-      .catch(err => {
-        res.status(500).json({ error: "Internal server error" });
-      });
+  notesRouter.delete("/:id", async (req, res) => {
+    try {
+      await notesService.deleteOneById(req.params.id);
+      res.status(200).json({});
+    } catch (err) {
+      res.status(500).json({ error: "Internal server error" });
+    }
   });
 
-  notesRouter.put("/:id", (req, res) => {
-    notesService
-      .updateOneById(req.params.id, req.body)
-      .then(note => res.status(200).json({ note }))
-      .catch(err => {
-        return res.status(500).json({ error: "Internal server error" });
-      });
+  notesRouter.put("/:id", async (req, res) => {
+    try {
+      const note = await notesService.updateOneById(req.params.id, req.body);
+      res.status(200).json({ note });
+    } catch (err) {
+      return res.status(500).json({ error: "Internal server error" });
+    }
   });
   return notesRouter;
 };
